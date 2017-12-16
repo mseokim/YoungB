bb3f5bb1d08153517834f8f358a7e3f0582ac0bf
mseokim <min101110001@icloud.com>
2017-12-16 16:05:28 +0900

    first commint

---
+++ YoungB/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  YoungB
+//
+//  Created by 김민서 on 2017. 12. 16..
+//  Copyright © 2017년 김민서. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    func setUI() {
+        
+    }
+}
+
